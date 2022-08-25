0f3ed2c33f6d5b359b8d521bd2511d97dd801082
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2022-08-25 10:28:40 +0300

    update documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,19 @@
+//! # kickasstorrent crate
+//!
+//! <p>
+//! <a href="https://crates.io/crates/kickasstorrent" target="_blank">
+//!  <img src="https://img.shields.io/crates/v/kickasstorrent.svg" />
+//! </a>
+//! <a href="https://docs.rs/kickasstorrent" target="_blank">
+//!  <img src="https://docs.rs/kickasstorrent/badge.svg" />
+//! </a>
+//! <a href="LICENSE" target="_blank">
+//!  <img alt="License: MIT" src="https://img.shields.io/badge/License-MIT-blue.svg" />
+//! </a>
+//! </p>
+//!
+//! KickassTorrents CLI is a command line interface and Rust library for KickassTorrents.
+
 pub mod constants;
 pub mod formater;
 pub mod parser;
